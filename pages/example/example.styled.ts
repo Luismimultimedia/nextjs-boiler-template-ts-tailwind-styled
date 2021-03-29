37e2d9b0d8fd52fcc9b65983cb9e18d1dac155fa
Luismimultimedia <luismimultimedia@gmail.com>
2021-03-28 22:28:07 -0500

    feat: add twin macros config

---
+++ pages/example/example.styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import tw from "twin.macro"
+
+export const ExampleTitle = styled.h1.attrs({
+    className: "ExampleTitle"
+})`
+    ${tw`text-red-500`}
+`
---
+++ pages/example/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Styles
+import { ExampleTitle } from './example.styled'
+
+const Example = () => {
+    return (
+        <div>
+            <ExampleTitle>Hello example</ExampleTitle>
+        </div>
+    )
+}
+
+export default Example
